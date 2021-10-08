f48d3543ef6341256fa3851e9d49f2817bcca688
MAbdelAziz <mohamad.masoud93@gmail.com>
2021-10-09 01:16:25 +0200

    -Initial setup for server.js -Server listening on port 8000
    -API key and URL added to app.js as const global variables
    
    Signed-off-by: MAbdelAziz <mohamad.masoud93@gmail.com>

--- website/app.js
+++ website/app.js
@@ -1,4 +1,6 @@
 /* Global Variables */
+const API_URL = "api.openweathermap.org/data/2.5/weather?q="
+const API_KEY = "&appid=37849b115c3a8e5531fa6a07d6db9de6"
 
 // Create a new date instance dynamically with JS
 let d = new Date();
